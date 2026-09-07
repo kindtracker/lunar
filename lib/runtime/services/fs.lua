154cfda30765d5581405a4a983111a9101b52a2d
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-09-07 11:50:35 -0300

    upd: add attrs to files upd: change file's parent to folder

--- lib/runtime/services/fs.lua
+++ lib/runtime/services/fs.lua
@@ -10,14 +10,43 @@ function FileSystemModule.__Lunar_Internal__Init__(instance)
   FileSystemService = Instance.new()
   FileSystemService.Name = "FileSystemService"
 
+  function FileSystemModule.__Lunar_Internal__Convert_Attrs__(Path, Name, Attributes)
+    local ModeList = {
+      ["directory"] = "Folder",
+      ["file"] = "File",
+      ["socket"] = "Socket",
+      ["named pipe"] = "NamedPipe",
+      ["char device"] = "CharDevice",
+      ["block device"] = "BlockDevice",
+      ["other"] = "Unknown"
+    }
+    local Lunar_Attributes = {
+      Name = Name,
+      Path = Path,
+      Type = ModeList[Attributes.mode],
+      Size = Attributes.size,
+      ModificationTime = Attributes.modification,
+      AccessTime = Attributes.access,
+      ChangeTime = Attributes.change,
+      Permissions = Attributes.permissions
+    }
+
+    for key, value in pairs(Attributes) do
+      Lunar_Attributes["__Hidden_LFS__" .. key] = value
+    end
+  end
+
   function FileSystemService:GetFolder(Path, Recursive)
     Recursive = Recursive or false
 
     local Folder = Instance.new()
     local Files = lfs.dir(Path)
     for fileName in Files do
+      local Attributes = FileSystemService:__Lunar_Internal__Convert_Attrs__(Path, fileName, lfs.attributes(Path .. "/" .. fileName))
       local IFile = Instance.new()
-      IFile.Name = Path
+      IFile.Name = fileName
+      IFile.Attributes = Attributes
+      IFile.Parent = Folder
     end
   end
 
