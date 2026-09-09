abff8242c1d271398d0809d81a74916965a958db
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-09-09 19:26:35 -0300

    upd: add heartbeat in runservice's example

--- examples/runservice/main.lua
+++ examples/runservice/main.lua
@@ -2,7 +2,11 @@ local RunService = Lunar:GetService("RunService")
 local Task = Lunar:GetService("TaskService")
 
 RunService.Stepped:Connect(function(DeltaTime)
-  print(DeltaTime, 1/DeltaTime)
+  print("Stepped", DeltaTime, 1/DeltaTime)
+end)
+
+RunService.Heartbeat:Connect(function(DeltaTime)
+  print("Heartbeat", DeltaTime, 1/DeltaTime)
 end)
 
 while true do
