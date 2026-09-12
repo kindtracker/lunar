7b1ccbec617be49df99033193337c95f40f1428d
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-09-11 22:27:53 -0300

    dupd: add HCS.RequestOptions and HCS:Get(Url)

--- lib/runtime/services/http-client.lua
+++ lib/runtime/services/http-client.lua
@@ -1,4 +1,5 @@
 local http = require("socket.http")
+local ltn12 = require("ltn12")
 
 local HttpClientModule = {}
 local HttpClientService
@@ -10,10 +11,12 @@ function HttpClientModule.__Lunar_Internal__Init__(instance)
   HttpClientService = Instance.new("Service")
   HttpClientService.Name = "HttpClientService"
 
-  function HttpClientService:Request(RequestOptions)
-    return http.request({
-      url = RequestOptions.Url,
-      headers = RequestOptions.Headers,
+  HttpClientService.RequestOptions = {}
+
+  function HttpClientService:__Lunar_Internal__Convert_RequestOptions__(RequestOptions, NoUrl, Method)
+    return {
+      url = NoUrl and nil or RequestOptions.Url,
+      headers = Method or RequestOptions.Headers,
       method = RequestOptions.Method,
       proxy = RequestOptions.Proxy,
       redirect = RequestOptions.FollowRedirect or true,
@@ -21,7 +24,25 @@ function HttpClientModule.__Lunar_Internal__Init__(instance)
       create = RequestOptions.__Hidden_socket_http__create,
       step = RequestOptions.__Hidden_socket_http__step,
       source = RequestOptions.__Hidden_socket_http__source,
-    })
+    }
+  end
+
+  function HttpClientService:Request(Url)
+    if Url then
+      HttpClientService.RequestOptions.Url = Url
+    end
+    return http.request(
+      HttpClientService:__Lunar_Internal__Convert_RequestOptions__(HttpClientService.RequestOptions, true, nil)
+    )
+  end
+
+  function HttpClientService:Get(Url)
+    if Url then
+      HttpClientService.RequestOptions.Url = Url
+    end
+    return http.request(
+      HttpClientService:__Lunar_Internal__Convert_RequestOptions__(HttpClientService.RequestOptions, true, "GET")
+    )
   end
 
   return HttpClientService
