cee47548e61e5979f1eef80682195b3203f4d2e4
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-09-14 16:14:21 -0300

    add more functions to httpsharedservice

--- lib/runtime/services/http-shared.lua
+++ lib/runtime/services/http-shared.lua
@@ -1,3 +1,5 @@
+local neturl = require("url")
+
 local HttpSharedModule = {}
 local HttpSharedService
 local Instance
@@ -9,6 +11,63 @@ function HttpSharedModule.__Lunar_Internal__Init__(instance, jsonservice)
 
   HttpSharedService = Instance.new("Service")
   HttpSharedService.Name = "HttpSharedService"
+
+  function HttpSharedService:QueryDecode(QueryString)
+    return neturl.parseQuery(QueryString)
+  end
+
+  function HttpSharedService:QueryEncode(QueryTable)
+    return neturl.buildQuery(QueryTable)
+  end
+
+  function HttpSharedService:UrlParse(Url)
+    local NUTable = neturl.parse(Url)
+    return {
+      Scheme = NUTable.scheme,
+      UserInfo = NUTable.userinfo,
+      User = NUTable.user,
+      Password = NUTable.password,
+      Authority = NUTable.authority,
+      Host = NUTable.host,
+      Port = NUTable.port,
+      Path = NUTable.path,
+      Query = NUTable.query,
+      Fragment = NUTable.fragment,
+    }
+  end
+
+  function HttpSharedService:UrlBuild(Url)
+    return neturl.parse(Url):build()
+  end
+
+  function HttpSharedService:UrlNormalize(Url)
+    return neturl.parse(Url):normalize()
+  end
+
+  function HttpSharedService:UrlAddSegment(Url, Segment)
+    return neturl.parse(Url) / Segment
+  end
+
+  function HttpSharedService:UrlRemoveDotSegments(Path)
+    return neturl.removeDotSegments(Path)
+  end
+
+  function HttpSharedService:UrlResolve(BaseUrl, RelativeUrl)
+    local Base = neturl.parse(BaseUrl)
+    return tostring(Base:resolve(RelativeUrl))
+  end
+
+  function HttpSharedService:UrlSetQuery(Url, Query)
+    local URLObject = neturl.parse(Url)
+    URLObject:setQuery(Query)
+    return URLObject:build()
+  end
+
+  function HttpSharedService:UrlSetAuthority(Url, Authority)
+    local URLObject = neturl.parse(Url)
+    URLObject:setAuthority(Authority)
+    return URLObject:build()
+  end
 
   function HttpSharedService:JSONEncode(Table)
     return JSONService:Encode(Table)
