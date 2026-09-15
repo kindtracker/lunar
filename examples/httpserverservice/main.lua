d25027948a031a30ec97701202bc865bdca0458b
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-09-15 20:06:45 -0300

    add Use and make custom static files (currently insecure)

--- examples/httpserverservice/main.lua
+++ examples/httpserverservice/main.lua
@@ -6,11 +6,16 @@ local Console = Lunar:GetService("ConsoleService")
 local Server = HttpServerService:Create({
   Port = 8080,
   Host = "127.0.0.1",
-  Location = "Workspace",
+  Location = "Workspace/",
 })
 
+Server:Use(function(Request, Response, Next)
+  Console:Log("Request.Path: %s", Request.Path)
+  Next()
+end)
+
 Server:Route("GET", "/hello", function(Request, Response)
-  Console:Log("%s", Request.Path)
+  Console:Log("%s", "Hellllo user!")
   Response:Write("<h1>Helllo!</h1>")
   Response:Close()
 end)
